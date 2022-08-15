9af23250983a6d0a5649673c3b712bc1fc0aea6b
rahulchoudhary7 <76811033+rahulchoudhary7@users.noreply.github.com>
2022-08-15 18:43:54 +0530

    Time: 400 ms (48.08%), Space: 119.7 MB (44.62%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,68 +10,21 @@
  */
 class Solution {
 private:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = NULL;
-        ListNode* fwd = NULL;
-        
-        ListNode* curr = head;
-        while(curr){
-            fwd = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = fwd;
+    
+    int maxi = INT_MIN;
+    ListNode* front = NULL;
+    
+    void maX(ListNode* head){
+        if(head){
+            maX(head->next);
+            maxi = max(maxi, head->val + front->val);
+            front = front->next;
         }
-        return prev;
     }
 public:
     int pairSum(ListNode* head) {
-       
-        
-//         vector<int> LL;
-//         while(curr){
-//             LL.push_back(curr->val);
-//             curr = curr->next;
-//         }
-        
-//         int maxi = INT_MIN;
-        
-//         int i = 0; 
-//         int j = LL.size() -1;
-        
-//         while(i<j){
-//             int twinSum = LL[i++]+LL[j--];
-//             maxi = max(maxi, twinSum);
-//         }
-//         return maxi;
-        
-        int maxTwinSum = INT_MIN;
-        
-        
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-        ListNode* fast = head->next;
-        ListNode* slow = head;
-              
-        while(fast && fast->next){
-            fast = fast->next->next;
-            slow = slow->next;
-        }
-        
-        ListNode* rCurr = reverseList(slow->next);
-        
-        // ListNode* stopcheck = rCurr;
-        
-        while(rCurr){
-            int sum = curr->val + rCurr->val;
-            maxTwinSum = max(sum, maxTwinSum);
-            curr = curr->next;
-            rCurr = rCurr->next;
-        }
-        
-        
-        
-        return maxTwinSum;
-        
-        
+        front = head;
+        maX(head);
+        return maxi;
     }
 };
