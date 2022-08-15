3cf738eb4b727ae5747db657321310b0563f4f54
rahulchoudhary7 <76811033+rahulchoudhary7@users.noreply.github.com>
2022-08-15 14:28:12 +0530

    Time: 450 ms (31.51%), Space: 148.1 MB (5.13%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -9,24 +9,88 @@
  * };
  */
 class Solution {
-public:
-    int pairSum(ListNode* head) {
+private:
+    ListNode* newLL(ListNode* &head, ListNode* &dummy){
+        ListNode* prev = NULL;
+        ListNode* fwd = NULL;
+        
         ListNode* curr = head;
-        vector<int> LL;
+        ListNode* newCurr = dummy;
         while(curr){
-            LL.push_back(curr->val);
+            
+            ListNode* temp = new ListNode(curr->val);
+            newCurr->next = temp;
+            newCurr = temp;
+            
             curr = curr->next;
         }
+        return reverse(dummy->next);
+    }
+    
+    ListNode* reverse(ListNode* & head){
+        ListNode* prev = NULL;
+        ListNode* fwd = NULL;
         
-        int maxi = INT_MIN;
-        
-        int i = 0; 
-        int j = LL.size() -1;
-        
-        while(i<j){
-            int twinSum = LL[i++]+LL[j--];
-            maxi = max(maxi, twinSum);
+        ListNode* curr = head;
+        while(curr){
+            fwd = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = fwd;
         }
-        return maxi;
+        return prev;
+    }
+public:
+    int pairSum(ListNode* head) {
+       
+        
+//         vector<int> LL;
+//         while(curr){
+//             LL.push_back(curr->val);
+//             curr = curr->next;
+//         }
+        
+//         int maxi = INT_MIN;
+        
+//         int i = 0; 
+//         int j = LL.size() -1;
+        
+//         while(i<j){
+//             int twinSum = LL[i++]+LL[j--];
+//             maxi = max(maxi, twinSum);
+//         }
+//         return maxi;
+        
+        int maxTwinSum = INT_MIN;
+        
+        
+        ListNode* curr = head;
+        
+//         ListNode* fast = head->next;
+//         ListNode* slow = head;
+        
+//         while(fast && fast->next){
+//             fast = fast->next->next;
+//             slow = slow->next;
+//         }
+        ListNode* dummy = new ListNode(-1);
+        
+        ListNode* rCurr = newLL(head, dummy);
+        
+        
+        while(curr && rCurr){
+            int sum = curr->val + rCurr->val;
+            maxTwinSum = max(sum, maxTwinSum);
+            
+            curr = curr->next;
+            rCurr = rCurr->next;
+        }
+        
+        
+        
+        
+        return maxTwinSum;
+        
+        
     }
 };
