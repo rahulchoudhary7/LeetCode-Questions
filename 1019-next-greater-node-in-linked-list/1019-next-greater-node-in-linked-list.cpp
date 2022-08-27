642f3f8adcfb6e4c67bad6cf03dd635817c81ecd
rahulchoudhary7 <76811033+rahulchoudhary7@users.noreply.github.com>
2022-08-27 21:54:06 +0530

    Time: 239 ms (26.69%), Space: 37.9 MB (99.37%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -9,23 +9,62 @@
  * };
  */
 class Solution {
+private:
+    ListNode* reverse(ListNode* curr, int &count){
+        ListNode* prev = NULL;
+        while(curr){
+            count++;
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
 public:
     vector<int> nextLargerNodes(ListNode* head) {
-        vector<int> ans;
+        // vector<int> ans;
         
-        ListNode* curr = head;
+        // ListNode* curr = head;
         
-        while(curr){
-            ListNode* temp = curr;
-            while(curr!=NULL && curr->val <= temp->val){
-                curr = curr->next;
-            }
-            if(curr)
-                ans.push_back(curr->val);
-            else
-                ans.push_back(0);
-            curr = temp->next;
+        // while(curr){
+        //     ListNode* temp = curr;
+        //     while(curr!=NULL && curr->val <= temp->val){
+        //         curr = curr->next;
+        //     }
+        //     if(curr)
+        //         ans.push_back(curr->val);
+        //     else
+        //         ans.push_back(0);
+        //     curr = temp->next;
+        // }
+        
+        int count = 0;
+        ListNode* newHead = reverse(head, count);
+        
+        ListNode* temp = newHead;
+        
+        while(temp){
+            cout<<temp->val;
+            temp = temp->next;
         }
+        
+        vector<int> ans(count);
+        int n = count-1;
+        ListNode* curr = newHead;
+        stack<int> s;
+        s.push(0);
+        
+        while(n>=0 && curr){
+            while(s.top()!=0 && s.top() <= curr->val){
+               s.pop();
+            }
+            ans[n--] = s.top();
+            s.push(curr->val);
+            curr = curr->next;
+        }
+        
         return ans;
     }
 };
