ba77a139d32d1c7128e4290f42dc4a9150c9d23b
rahulchoudhary7 <76811033+rahulchoudhary7@users.noreply.github.com>
2022-08-29 12:28:57 +0530

    Time: 74 ms (25.45%), Space: 24.4 MB (31.37%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -4,17 +4,30 @@ public:
         int n = nums.size();
         vector<int> ans(n);
         
-        for(int i=0; i<n; i++){
-            int j = (i+1)%n;
-            while(j!=i){
-                if(nums[j]>nums[i]){
-                    ans[i] = nums[j];
-                    break;
-                }
+//         for(int i=0; i<n; i++){
+//             int j = (i+1)%n;
+//             while(j!=i){
+//                 if(nums[j]>nums[i]){
+//                     ans[i] = nums[j];
+//                     break;
+//                 }
                 
-                j = (j+1)%n;
+//                 j = (j+1)%n;
+//             }
+//             if(j==i) ans[i] = -1;
+//         }
+        stack<int> s;
+        for(int i=n-1; i>=0; i--){
+            s.push(nums[i]);
+        }
+        
+        for(int i=n-1; i>=0; i--){
+            while(!s.empty() && s.top()<=nums[i]){
+                s.pop();
             }
-            if(j==i) ans[i] = -1;
+            if(!s.empty())ans[i] = s.top();
+            else ans[i] =-1;
+            s.push(nums[i]);
         }
         return ans;
     }
