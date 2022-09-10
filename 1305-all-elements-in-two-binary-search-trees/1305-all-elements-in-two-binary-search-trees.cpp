5f96d6d78d92f0ab0ee3153904035f9b0f039014
rahulchoudhary7 <76811033+rahulchoudhary7@users.noreply.github.com>
2022-09-10 17:57:23 +0530

    Time: 181 ms (83.66%), Space: 85.4 MB (43.85%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -17,13 +17,39 @@ private:
         ans.push_back(root->val);
         inorder(root->right, ans);
     }
+    
+    vector<int> merge(vector<int> &a, vector<int> &b){
+        int n = a.size();
+        int m = b.size();
+        vector<int> ans(m+n);
+        
+        int i(0), j(0), ansIdx(0);
+        
+        while(i<n && j<m){
+            if(a[i]<b[j]){
+                ans[ansIdx++] = a[i++];
+            }
+            else{
+                ans[ansIdx++] = b[j++];
+            }
+        }
+        
+        while(i<n){
+            ans[ansIdx++] = a[i++];
+        }
+        while(j<m){
+            ans[ansIdx++] = b[j++];
+        }
+        return ans;
+    }
 public:
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> ans;
-        inorder(root1, ans);
-        inorder(root2, ans);
+        vector<int> list1;
+        vector<int> list2;
+        inorder(root1, list1);
+        inorder(root2, list2);
         
-        sort(ans.begin(),ans.end());
+        vector<int> ans = merge(list1, list2);
         return ans;
     }
 };
